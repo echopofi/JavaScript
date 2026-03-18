f71b926d26724404a86103dfff4cc621e7bbb55e
Echo-007-sudo <bosspofii@gmail.com>
2026-03-18 14:08:29 +0100

    feat: added spread operator example

--- week9/wednesday/wednesday.js
+++ week9/wednesday/wednesday.js
@@ -1,8 +1,9 @@
 //primitive data types are stored in the stack ad the stack is in your RAM beceause their size is been known at rhr e run-time, in compiled languagies
 //learn Hi\eap an Stack
 // 
-var person = {
+let person = {
     name: "John",
+    name: "Jill", //this is called over-ride, as js over rides John
     age: 30,
     city: "Jos",
     isMarried: false,
@@ -17,5 +18,9 @@ var person = {
 };
 person.greet();
 console.log(person.address.work);
-person.name = "Jack Sparrow";
+//person.name = "Jack Sparrow";
 console.log(person.name);
+
+//spread operator is used to add items to an object
+person = { ...person, country: "Nigeria", state: "Plateau", nextOfKin: "Jane Doe" };
+console.log(person);
